c1ce765bfe8c77225357af31c58d893806a135ec
AlliesChen <cyphuaenng@pm.me>
2021-11-20 00:45:12 +0800

    Support basic operation

--- js-file.js
+++ js-file.js
@@ -1,5 +1,62 @@
+const monitor = document.querySelector('.screen');
+const digits = document.querySelectorAll('.digit');
+const operators = document.querySelectorAll('.operator');
+const clear_btn = document.getElementById('clear');
+const operation = ['0'];
+
+// Show digits that user clicked on the monitor
+digits.forEach((element) => {
+    element.addEventListener('click', (e) => {
+        (operation[0] === '0') ? operation[0] = e.target.textContent : operation[0] += e.target.textContent;
+        monitor.textContent = operation[0];
+    })
+});
+
+operators.forEach((element) => {
+    element.addEventListener('click', (e) => {
+        if (!operation[1] || operation[1] === '=') {
+            operation[1] = e.target.textContent;
+            operation[0] = '';
+        } else if (operation[1] != '=' && e.target.textContent != '=') {
+            operation[1] = e.target.textContent;
+            operation[0] = '';
+        } else if (e.target.textContent === '=') {
+            operation[0] = operation[0] || monitor.textContent;
+            monitor.textContent = operate(operation[2], operation[1], operation[0]);
+        } else {
+            monitor.textContent = operate(operation[2], operation[1], operation[0]);
+        }
+        operation[2] = monitor.textContent;
+    });
+});
+
+// Clear the current input
+clear_btn.addEventListener('click', () => {
+    operation[0] = '0';
+})
+
+// function: divide
+function divideNums(dividend, divisor) {
+    return dividend / divisor;
+}
+
+// function: multiply
+function multiplyNums(multiplier, multiplicand) {
+    return multiplier * multiplicand;
+}
+
+// function: substract
+function substrNums(minuend, subtrahend) {
+    return minuend - subtrahend;
+}
+
+// function: add
+function addNums(initial, addend) {
+    return Number(initial) + Number(addend);
+}
+
 // function: operate
-function operate(operator, initial, value) {
+function operate(initial, operator, value) {
     switch (operator) {
         case '+':
             return addNums(initial, value);
@@ -13,23 +70,4 @@ function operate(operator, initial, value) {
             return 'Error: issue found on operate'
     }
 
-}
-
-// function: add
-function addNums(initial, addend) {
-    return initial + addend;
-}
-
-// function: substract
-function substrNums(minuend, subtrahend) {
-    return minuend - subtrahend;
-}
-// function: multiply
-function multiplyNums(multiplier, multiplicand) {
-    return multiplier * multiplicand;
-}
-
-// function: divide
-function divideNums(dividend, divisor) {
-    return dividend / divisor;
 }
