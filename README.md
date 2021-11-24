2d54d4c740dece0ded28df26d8c9f7e2d67ff145
AlliesChen <cyphuaenng@pm.me>
2021-11-24 13:02:47 +0800

    The prototype

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simple Calculator
+
+Practices from [The Odin Project](https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/calculator)
+
+## Reference
+
+[Letter-spacing - 金魚都能懂的CSS必學屬性](https://ithelp.ithome.com.tw/articles/10244794) on iT邦幫忙
+
+[深入 CSS 之 line-height 應用](https://muki.tw/tech/css-line-height/) on MUKI space
+
+### Fonts
+
+**Google fonts:**
+
+Screen display: [ZCOOL QingKe HuangYou](https://fonts.google.com/share?selection.family=Comfortaa%7CZCOOL%20QingKe%20HuangYou)  Designed by ZCOOL, Zheng Qingke
+
+Keyboard: [Comfortaa](https://fonts.google.com/share?selection.family=Comfortaa%7CZCOOL%20QingKe%20HuangYou) Designed by Johan Aakerlund 
--- js-file.js
+++ js-file.js
@@ -1,50 +1,63 @@
 const monitor = document.querySelector('.screen');
+const decimal_btn = document.querySelector('.decimal');
 const digits = document.querySelectorAll('.digit');
 const operators = document.querySelectorAll('.operator');
 const clear_btn = document.getElementById('clear');
 const allClear_btn = document.getElementById('allClear');
-const operation = ['0'];
+const operation = [];
 
-// Show digits that user clicked on the monitor
+// When user use the digits plate
 digits.forEach((element) => {
     element.addEventListener('click', (e) => {
-        while (monitor.textContent === 'E') {
-            return 2;
+        // When an error came from trying to divide a number by 0
+        if (monitor.textContent === 'E') return 10;
+        // Check if any operation is on pending or has been completed
+        if (operation[0]) {
+            operation[2] = operation[0]; // Commit the stored number to an repository
+            operation[0] = ''; // Reset the buffer
+            monitor.textContent = '0'; // Reset the display
+        } else if (operation[1] === '=') { // For a completed operation
+            monitor.textContent = '0'; // Reset the display
+            operation[1] = ''; // Reset the operator buffer
         }
-        (operation[0] === '0') ? operation[0] = e.target.textContent : operation[0] += e.target.textContent;
-        monitor.textContent = operation[0];
+        // Make the calculator a 12-digit limited
+        if (checkInputLength()) return 12;
+        // As the screen display 0 in default, Write over it first, and then do concatenation
+        (monitor.textContent === '0') ? monitor.textContent = e.target.textContent : monitor.textContent += e.target.textContent;
     })
 });
 
 operators.forEach((element) => {
     element.addEventListener('click', (e) => {
-        if (!operation[1] || operation[1] === '=') {
-            operation[1] = e.target.textContent;
-            operation[0] = '';
-        } else if (operation[1] != '=' && e.target.textContent != '=') {
-            monitor.textContent = operate(operation[2], operation[1], operation[0]);
-            operation[1] = e.target.textContent;
-            operation[0] = '';
-        } else if (e.target.textContent === '=') {
-            operation[0] = operation[0] || monitor.textContent;
-            monitor.textContent = operate(operation[2], operation[1], operation[0]);
-        } else {
-            monitor.textContent = operate(operation[2], operation[1], operation[0]);
+        // Store the number showing on the monitor to a buffer
+        operation[0] = monitor.textContent;
+        // Check if there is a completed operation
+        if (operation[1] === '=') {
+            operation[1] = ''; // Reset the operator buffer
         }
-        while (monitor.textContent != 'E') {
-            operation[2] = monitor.textContent;
-            return 1;
+        // Check if an operation is on pending
+        if (operation[2]) {
+            operation[2] = operate(operation[2], operation[1], operation[0]);
+            monitor.textContent = operation[2]; // Show the result on the display
+            operation[2] = ''; // Reset the repository
+        }
+        // Store an operator to a buffer
+        if (e.target.textContent != '=') {
+            operation[1] = e.target.textContent;
+            operation[0] = monitor.textContent; // Store the result for next operation
+        } else {
+            operation[1] = '=';
+            operation[0] = ''; // Reset the buffer
         }
     });
 });
 
-// Clear the current input
+// Button: clear the current input
 clear_btn.addEventListener('click', () => {
-    operation[0] = '0';
-    monitor.textContent = operation[0];
+    monitor.textContent = '0';
 })
 
-// Clear all the input
+// Button: clear all the input
 allClear_btn.addEventListener('click', () => {
     for (let i = 0; i < 3; i++) {
         operation[i] = '';
@@ -52,44 +65,104 @@ allClear_btn.addEventListener('click', () => {
     monitor.textContent = '0';
 })
 
+// Button: apply floating point to the number
+decimal_btn.addEventListener('click', () => {
+    if(monitor.textContent.includes('.')) {
+        return 0;
+    } else {
+        monitor.textContent += '.'
+        return 1;
+    }
+});
+
+// **Functions**
+
+// Make the string value 12-digit at most
+function checkOutputLength(value) {
+    let k = Number(value);
+    // Count the length without the negative symbol
+    let l = (value.startsWith('-')) ? value.length - 1 : value.length;
+    // If the number with a float point
+    if (k % 1 != 0 && l > 13) {
+        let i = value.slice(0, value.indexOf('.')).length;
+        return k.toFixed(12 - i);
+    } else if (l > 12) {
+        return k.toExponential();
+    } else {
+        return k;
+    }
+}
+
+function checkInputLength() {
+    // If there isn't an operation on pending
+    if (!operation[1]) {
+        return (monitor.textContent.includes('.')) ? monitor.textContent.length > 12 : monitor.textContent.length > 11;
+    } else if (!operation[0]) { // For the operation that needs the other operand
+        return (monitor.textContent.includes('.')) ? monitor.textContent.length > 12 : monitor.textContent.length > 11;
+    }
+}
+
+function calcFloat(value1, value2, operator) {
+    let prec = Math.max(value1.slice(value1.indexOf('.')).length, value2.slice(value2.indexOf('.')).length) - 1;
+    value1 *= Math.pow(10, prec);
+    value2 *= Math.pow(10, prec);
+    if (operator === '/') {
+        return operate(`${value1}`, operator, `${value2}`);
+    } else {
+        return operate(`${value1}`, operator, `${value2}`) / Math.pow(10, prec);
+    }
+}
+
 // function: divide
-function divideNums(dividend, divisor) {
+function divideNums(dividend, divisor, operator) {
     if (divisor == 0) {
         // Divide by 0 is undifined')
         return 'E';
+    } else if (dividend.includes('.') || divisor.includes('.')) {
+        return calcFloat(dividend, divisor, operator);
     } else {
-        return dividend / divisor;
+        let num = dividend / divisor;
+        let n = num.toFixed(11);
+        while (n.endsWith(0)) {
+            n = n.slice(0, -1);
+        }
+        return checkOutputLength(n);
     }
 }
 
 // function: multiply
 function multiplyNums(multiplier, multiplicand) {
-    return multiplier * multiplicand;
+    let n = multiplier * multiplicand;
+    return checkOutputLength(n.toString());
 }
 
 // function: substract
-function substrNums(minuend, subtrahend) {
-    return minuend - subtrahend;
+function substrNums(minuend, subtrahend, operator) {
+    if (minuend.includes('.') || subtrahend.includes('.')) return calcFloat(minuend, subtrahend, operator);
+    let k = minuend - subtrahend; 
+    return checkOutputLength(k.toString());
 }
 
 // function: add
-function addNums(initial, addend) {
-    return Number(initial) + Number(addend);
+function addNums(initial, addend, operator) {
+    if (initial.includes('.') || addend.includes('.')) return calcFloat(initial, addend, operator);
+    let k = Number(initial) + Number(addend);
+    return checkOutputLength(k.toString());
 }
 
 // function: operate
 function operate(initial, operator, value) {
     switch (operator) {
         case '+':
-            return addNums(initial, value);
+            return addNums(initial, value, operator);
         case '-':
-            return substrNums(initial, value);
+            return substrNums(initial, value, operator);
         case '*':
             return multiplyNums(initial, value);
         case '/':
-            return divideNums(initial, value);
+            return divideNums(initial, value, operator);
         default:
-            return 'Error: no such operator in function "operate"'
+            return '01'
     }
 
 }
